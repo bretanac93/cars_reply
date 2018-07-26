58b24a8a728a76a94ebcc3ca92b3025002e0d1de
Cesar Bretana Gonzalez <bretanac@gmail.com>
2018-07-26 13:20:24 +0200

    Create a new demand

--- src/controllers/demands.js
+++ src/controllers/demands.js
@@ -20,7 +20,31 @@ exports.getById = (req, res) => {
 }
 
 exports.save = (req, res) => {
-    res.send('Not implemented');
+    // new Date('1993-04-25 13:00')
+    let {
+        pickup_date,
+        dropoff_date,
+        pickup_location,
+        dropoff_location,
+        user,
+        car,
+    } = req.body;
+
+    let demand = new Demand({
+        pickup_date: new Date(pickup_date),
+        dropoff_date: new Date(dropoff_date),
+        pickup_location,
+        dropoff_location,
+        user,
+        car,
+    });
+
+    demand.save((err, data) => {
+        if (err)
+            res.status(400);
+        
+        res.json({ data });
+    })
 }
 
 exports.update = (req, res) => {
